233b82ca581d4879dd4a73c8873aae25a5570701
Anton W1nsh <antonpavlovich2303@gmail.com>
2025-12-12 01:13:07 +0500

    Add border, color themes, validation collide with walls(curses.error)

---
+++ config.py
@@ -0,0 +1,2 @@
+base_of_color_theme = 'green'
+background_of_color_theme = 'black'
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import curses, time, random
+import config
 
 def spawn_snake(screen):
-	for i in range(3):
-		screen.addch(10, i+1, curses.ACS_BLOCK)
+	scorer(screen, 0)
+	for i in range(3, 6):
+		screen.addch(10, i, curses.ACS_BLOCK, curses.color_pair(1))
 		screen.refresh()
 		time.sleep(0.25)
-	snake = [(10, 1), (10, 2), (10, 3)]
+	snake = [(10, 3), (10, 4), (10, 5)]
 	return snake
 
 def change_move_to(screen, move_to: str):
@@ -24,11 +26,11 @@ def change_move_to(screen, move_to: str):
 
 def generate_food(screen, snake):
 	while True:
-		food_y = random.randint(0, curses.LINES - 1)
-		food_x = random.randint(0, curses.COLS - 1)
+		food_y = random.randint(5, curses.LINES - 3)
+		food_x = random.randint(3, curses.COLS - 3)
 		food_yx = (food_y, food_x)
 		if food_yx not in snake:
-			screen.addch(food_y, food_x, curses.ACS_DIAMOND)
+			screen.addch(food_y, food_x, curses.ACS_DIAMOND, curses.color_pair(1))
 			screen.refresh()
 			time_spawn_food = time.time()
 			return food_yx, time_spawn_food
@@ -46,12 +48,14 @@ def move(screen, snake, move_to: str, food_yx, score: int):
 			new_head = (head_y, head_x - 1)
 		case 'right':
 			new_head = (head_y, head_x + 1)
-	if new_head in snake:
+	if (new_head in snake or 
+		new_head[0] in [0, 1, 2, 3, 4, curses.LINES, curses.LINES - 1, curses.LINES - 2] or 
+		new_head[1] in [0, 1, 2, curses.COLS, curses.COLS - 1, curses.COLS - 2]):
 		game_over(screen, score)
 		res['is_game'] = False
 		return res
 	snake.append(new_head)
-	screen.addch(new_head[0], new_head[1], curses.ACS_BLOCK)
+	screen.addch(new_head[0], new_head[1], curses.ACS_BLOCK, curses.color_pair(1))
 	if new_head != food_yx:
 		snake.pop(0)
 		screen.addch(old_tail_y, old_tail_x, ' ')
@@ -64,10 +68,10 @@ def move(screen, snake, move_to: str, food_yx, score: int):
 	
 def game_over(screen, score):
 	screen.clear()
-	screen.addstr("Game Over.\n")
-	screen.addstr(str(score))
+	screen.addstr("Game Over.\n", curses.color_pair(1))
+	screen.addstr(str(score), curses.color_pair(1))
 	screen.refresh()
-	time.sleep(5)
+	time.sleep(3)
 	screen.timeout(-1)
 	screen.getch()
 
@@ -82,14 +86,51 @@ def food_time(time_spawn_food):
 		return True
 	return False
 
+def border(screen):
+	range_cols = curses.COLS - 2
+	range_lines = curses.LINES - 2
+	screen.addstr(2, 3, 'Console-Snake. Developed by W1nsh.', curses.color_pair(1))
+	screen.addch(4, 2, curses.ACS_ULCORNER, curses.color_pair(1))
+	screen.addch(4, range_cols, curses.ACS_URCORNER, curses.color_pair(1))
+	screen.addch(range_lines, 2, curses.ACS_LLCORNER, curses.color_pair(1))
+	screen.addch(range_lines, range_cols, curses.ACS_LRCORNER, curses.color_pair(1))
+	for i in range(3, range_cols):
+		screen.addch(4, i, curses.ACS_HLINE, curses.color_pair(1))
+		screen.addch(range_lines, i, curses.ACS_HLINE, curses.color_pair(1))
+	for i in range(5, range_lines):
+		screen.addch(i, 2, curses.ACS_VLINE, curses.color_pair(1))
+		screen.addch(i, range_cols, curses.ACS_VLINE, curses.color_pair(1))
+	screen.refresh()
+
+def scorer(screen, score):
+	screen.addstr(2, 47, f'Score: {score}', curses.color_pair(1))
+	screen.refresh()
+
+def configer_color_theme():
+	colors_for_themes ={
+		'black': curses.COLOR_BLACK,
+		'blue': curses.COLOR_BLUE,
+		'cyan': curses.COLOR_CYAN,
+		'green': curses.COLOR_GREEN,
+		'magenta': curses.COLOR_MAGENTA,
+		'red': curses.COLOR_RED,
+		'white': curses.COLOR_WHITE,
+		'yellow': curses.COLOR_YELLOW
+	}
+	curses.init_pair(1, 
+				colors_for_themes.get(config.base_of_color_theme, curses.COLOR_MAGENTA), 
+				colors_for_themes.get(config.background_of_color_theme, curses.COLOR_BLACK))
+
 def game(screen):
 	is_game = True
+	border(screen)
 	snake = spawn_snake(screen)
 	generated_first_food = False
 	time_spawn_food = 0
 	score = 0
 	move_to = 'right'
 	while is_game:
+		scorer(screen, score)
 		move_to = change_move_to(screen, move_to)
 		if food_time(time_spawn_food):
 			if generated_first_food:
@@ -103,6 +144,7 @@ def game(screen):
 
 
 def main(screen):
+	configer_color_theme()
 	curses.update_lines_cols()
 	curses.curs_set(0)
 	game(screen)
